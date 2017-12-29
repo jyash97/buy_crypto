7f9d748cb4c5a137b2597655f380dfd69b4a2591
jyash97 <jyash97@gmail.com>
2017-12-29 23:05:59 +0530

    Functions added

--- src/Home.jsx
+++ src/Home.jsx
@@ -2,13 +2,15 @@ import React from 'react';
 
 import DisplayData from './DisplayData';
 import BuyCoin from './BuyCoin';
+import SellCoin from './SellCoin';
 
 class Home extends React.Component {
   constructor() {
     super();
     this.state = {
       prices: [],
-      initialPrice: 1000000
+      initialPrice: 1000000,
+      holdings: {}
     };
     this.handleRequest = this.handleRequest.bind(this);
     this.handleValue = this.handleValue.bind(this);
@@ -26,14 +28,33 @@ class Home extends React.Component {
     clearInterval(this.timer);
   }
 
-  handleValue(option = 'BTC', quantity = 1, event) {
-    this.handleRequest();
-    if (event.target.name === 'buy') {
+  handleValue(option, quantity = 1, event) {
+    if (event.target.name === 'sell') {
+      const gain =
+        this.state.initialPrice + this.state.prices[option] * quantity;
+      let b = this.state.holdings;
+      if (b[option] >= quantity) {
+        b[option] = (b[option] || 0) - parseFloat(quantity);
+        this.setState({
+          initialPrice: gain,
+          holdings: b
+        });
+      } else {
+        alert('Quantity to be sold is greater than the holdings');
+      }
+    } else {
       const left =
         this.state.initialPrice - this.state.prices[option] * quantity;
-      this.setState({
-        initialPrice: left
-      });
+      let a = this.state.holdings;
+      if (left > 0) {
+        a[option] = (a[option] || 0) + parseFloat(quantity);
+        this.setState({
+          initialPrice: left,
+          holdings: a
+        });
+      } else {
+        alert('You dont have enough Balance');
+      }
     }
   }
 
@@ -57,6 +78,10 @@ class Home extends React.Component {
           <React.Fragment>
             <DisplayData {...this.state.prices} />
             <BuyCoin
+              handleValue={this.handleValue}
+              data={Object.keys(this.state.prices)}
+            />
+            <SellCoin
               handleValue={this.handleValue}
               data={Object.keys(this.state.prices)}
             />
